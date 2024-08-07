e13d072eb1ac33bd5c9b79c3274c61c11e12f76b
mell <mell.zanatelli@gmail.com>
2024-08-07 16:07:25 -0300

    alterações

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,17 +1,53 @@
 import styled from "styled-components"
+import { colors } from "../../styles/GlobalStyle"
 
 export const LoginContainer = styled.div`
-    label{
+    label {
         padding-right: 10px;
+        color: ${colors.blue};
+        font-family: "Alatsi";
+        font-size: 1.5rem;
     }
-    input{
-        border: solid black 1px;
+    input, textarea {
+        border: solid ${colors.azul} 2px;
+        width: 100%;
     }
-    form{
-        padding-top: 10px;
-        padding-left: 10px;
+    form {
+        padding: 3rem;
+        align-items: center;
+        div {
+            padding: 1rem;
+        }
+        p{
+            align-items: center;
+            justify-content: center;
+            display: flex;
+            button{
+                align-items: center;
+                justify-content: center;
+            }
+        }
     }
-    button{
-        border: solid black 1px;
+    button {
+        padding: 0.5rem;
+        border-radius: 0.5rem;
+        background-color: ${colors.azul};
+        color: ${colors.branco};
+        font-family: "Alatsi";
+        font-size: 1.5rem;
     }
+    h1{
+        text-align: center;
+        color: ${colors.blue};
+        font-family: "Shrikhand";
+        font-size: 3rem;
+        width: 100%;
+        background-color: ${colors.laranjaC};
+    }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
 `
